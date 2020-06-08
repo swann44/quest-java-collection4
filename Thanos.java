149d05b50ed5782206451bc118414d70bdfc6668
swann <chevreauswann@outlook.fr>
2020-06-08 16:44:50 +0200

    quête sur map

--- Thanos.java
+++ Thanos.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Thanos {
 
     public static void main(String[] args) {
@@ -17,14 +19,39 @@ public class Thanos {
         Flower rose = new Flower("Rose");
 
         // TODO 1 : Create a TreeMap where :
+        Comparator<Hero> comparator = new Comparator<Hero>() {
+           @Override
+           public int compare(Hero orig, Hero compare) {
+               return orig.getName().compareTo(compare.getName());
+            }
+        };
+        Map<Hero, Flower> party = new TreeMap<>(comparator);
         // Hulk, Thor and Scarlet Witch have a Rose
+        party.put(hulk, rose);
+        party.put(thor, rose);
+        party.put(scarletWitch, rose);
         // Vision has a Tulip
+        party.put(vision, tulip);
         // Captain America has a Lily
+        party.put(captainAmerica, lily);
         // Doctor Strange and Black Widow have a Violet
+        party.put(doctorStrange, violet);
+        party.put(blackWidow, violet);
 
         // TODO 2 : Print if `begonia` is contained in the TreeMap
-
+        for (Hero hero : party.keySet()) {
+            Flower flower = party.get(hero);
+            if (flower == begonia) {
+                System.out.println(true);
+                break;
+            }
+            System.out.println(false);
+            break;
+        }
         // TODO 3 : For each hero, alphabetically, print the corresponding flower
-
+            for (Hero hero : party.keySet()) {
+                Flower flower = party.get(hero);
+                System.out.println(flower.getName());
+        }
     }
 }
